4f3d0721434f65c8572110757e1f2e587e2ac538
dcasUni <116928566+dcasUni@users.noreply.github.com>
2023-06-27 17:24:53 -0400

    Added Test Length Code

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+    returnLength();
+  }
+  
+  static public returnLength() {
+    Scanner input = new Scanner(System.in);
+    return input.nextLine().length();
   }
 }
